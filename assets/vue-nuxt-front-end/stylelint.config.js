d0c3b7b67c264135abcff31f49e84146cd4e0a44
Raphaël Goetter <raphael@goetter.fr>
2021-09-08 08:19:43 +0200

    Styleling config

--- assets/vue-nuxt-front-end/stylelint.config.js
+++ assets/vue-nuxt-front-end/stylelint.config.js
@@ -12,10 +12,19 @@ module.exports = {
     'selector-max-id': 0, // pas de sélecteur id
     'block-no-empty': true, // pas de block vide
     'comment-no-empty': true, // pas de commentaire vide
+    'property-no-unknown': true, // pas de propriété inconnue
     'no-duplicate-selectors': true, // pas de sélecteurs dupliqués
+    'max-nesting-depth': 3, // niveau d'imbrication max
     'color-no-hex': true, // pas de couleur en hexadécimal
+    'color-named': 'never', // pas de couleur nommée
+    'number-leading-zero': 'always', // toujous un zéro en début de décimale
+    'declaration-block-trailing-semicolon': 'always', // toujous un point-virgule
+    'no-irregular-whitespace': true, // pas de caractère espace irrégulier
+    'declaration-block-single-line-max-declarations': 1, // nombre de déclaration max par ligne
     'declaration-colon-space-before': 'never', // jamais d'espace avant le double-point
     'declaration-colon-space-after': 'always', // toujours une espace après le double-point
+    'selector-combinator-space-before': 'always', // // toujours une espace avant un combinateur
+    'selector-combinator-space-after': 'always', // // toujours une espace après un combinateur
     'rule-empty-line-before': 'always', // ligne vide entre deux blocs
     'max-empty-lines': 1, // une ligne maxi entre deux blocks
     'no-eol-whitespace': true, // pas de whitespace en fin de ligne
@@ -24,7 +33,7 @@ module.exports = {
     'at-rule-no-unknown': [
       true,
       {
-        'ignoreAtRules': ['function', 'if', 'each', 'include', 'mixin', 'extends', 'tailwind']
+        'ignoreAtRules': ['function', 'if', 'each', 'include', 'mixin', 'layer', 'extends', 'tailwind']
       }
     ]
   }
