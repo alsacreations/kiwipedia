62b1b63a2acd783c20d57fdb8991343ed6437d80
Nicolas Sutter <47984272+nicolassutter@users.noreply.github.com>
2021-10-18 16:23:43 +0200

    Update .eslintrc.js

--- assets/vue-nuxt-front-end/.eslintrc.js
+++ assets/vue-nuxt-front-end/.eslintrc.js
@@ -31,7 +31,7 @@ module.exports = {
     'quote-props': 'off', // controle des quotes autour des propriétés des objets
     'comma-dangle': ['error', 'never'], // pas de dernière virgule
     'no-trailing-spaces': 'warn', // pas d'espaces vides
-    semi: ['warn', 'never'], // pas de ";" à la fin des lignes
+    semi: ['error', 'never'], // pas de ";" à la fin des lignes
     'object-shorthand': 'warn',
     'space-infix-ops': ['warn'], // espaces entre opérateurs
     // les const, c'est la vie
