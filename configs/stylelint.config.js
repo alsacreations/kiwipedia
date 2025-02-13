012063a47eef2e1997fbfb975aba64d6e550ab59
Raphaël Goetter <raphael@goetter.fr>
2025-02-13 20:07:13 +0100

    style(config): mise à jour de la configuration de Stylelint pour améliorer les règles de style

--- configs/stylelint.config.js
+++ configs/stylelint.config.js
@@ -1,50 +1,47 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-html',
-    'stylelint-config-property-sort-order-smacss',
-  ],
-  plugins: ['stylelint-order'],
+  extends: ["stylelint-config-standard", "stylelint-config-html", "stylelint-config-property-sort-order-smacss"],
+  plugins: ["stylelint-order"],
   rules: {
-    // Sélécteurs
-    'selector-max-id': 0, // on refuse les ID
-    'selector-max-class': 3, // on limite le nombre de classes
-    'selector-max-type': 3, // on limite le nombre de sélecteurs d'éléments
-    'no-descending-specificity': null, // on désactive la règle de spécificité descendante
+    // Sélecteurs
+    "selector-max-id": 0, // on refuse les ID
+    "selector-max-class": 3, // on limite le nombre de classes
+    "selector-max-type": 3, // on limite le nombre de sélecteurs d'éléments
+    "no-descending-specificity": null, // on désactive la règle de spécificité descendante
 
     // Sélécteurs spécifiques
-    'selector-pseudo-class-no-unknown': [
+    "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["deep", "global"] }],
+    "selector-pseudo-element-no-unknown": [true, { ignorePseudoElements: ["v-deep"] }],
+    "at-rule-no-unknown": [true, { ignoreAtRules: ["theme", "utility"] }],
+    "declaration-property-value-no-unknown": [
       true,
-      { ignorePseudoClasses: ['deep', 'global'] },
+      { ignoreProperties: { animation: "auto", "animation-duration": "auto" } },
     ],
-    'selector-pseudo-element-no-unknown': [
-      true,
-      { ignorePseudoElements: ['v-deep'] },
-    ],
-    'at-rule-no-unknown': [true, { ignoreAtRules: ['theme', 'utility'] }],
-    'declaration-property-value-no-unknown': [
-      true,
-      { ignoreProperties: { animation: 'auto', 'animation-duration': 'auto' } },
-    ],
+
+    // Unités
+    "declaration-property-unit-allowed-list": {
+      "/^font|^font-size/": ["rem", "vw", "dvw"],
+    },
 
     // Imports
-    'import-notation': 'string', // pas de "url()" pour les imports
+    "import-notation": "string", // pas de "url()" pour les imports
 
     // Nesting
-    'max-nesting-depth': 3, // on limite la profondeur de l'imbrication
+    "max-nesting-depth": 3, // on limite la profondeur de l'imbrication
 
     // Media Queries
-    'media-feature-range-notation': 'context', // on force la notation moderne
+    "media-feature-range-notation": "context", // on force la notation moderne
 
     // Polices
-    'font-family-no-duplicate-names': null,
-    'font-weight-notation': 'numeric', // on force la notation numérique pour les poids de police
+    "font-family-no-duplicate-names": null,
+    "font-weight-notation": "numeric", // on force la notation numérique pour les poids de police
 
     // Couleurs
-    'color-hex-length': 'long', // on force la notation longue pour les couleurs hexadécimales
-    'color-named': 'never', // on refuse les couleurs nommées
-    'lightness-notation': 'percentage', // on force la notation en pourcentage pour la luminosité
-    'hue-degree-notation': 'number',
+    "color-hex-length": "long", // on force la notation longue pour les couleurs hexadécimales
+    "color-named": "never", // on refuse les couleurs nommées
+    "color-function-notation": "modern", // on force la notation moderne pour les fonctions de couleurs
+    "lightness-notation": "percentage", // on force la notation en pourcentage pour la luminosité
+    "alpha-value-notation": "percentage", // on force la notation en pourcentage pour l'alpha
+    "hue-degree-notation": "number",
   },
-}
+};
